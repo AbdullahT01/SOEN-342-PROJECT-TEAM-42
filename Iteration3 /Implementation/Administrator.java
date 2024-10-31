578a2aa7a1cad594b98001a7e81e3641eab4b6c2
Mustafa-M422 <158519759+Mustafa-M422@users.noreply.github.com>
2024-10-31 12:25:17 -0400

    Update and rename a to Administrator.java

---
+++ Iteration3 /Implementation/Administrator.java
@@ -0,0 +1,16 @@
+public class Administrator {
+    private String name;
+
+    public Administrator(String name) {
+        this.name = name;
+    }
+
+    public boolean createOffering(Offering offering) {
+        // Implementation to add the offering to the system
+        return true;
+    }
+
+    public void deleteAccount(Object user) {
+        // Implementation to delete a client or instructor account
+    }
+}
